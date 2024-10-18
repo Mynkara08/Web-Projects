ea8ec2a8394d66f9bc84cd2663d05c221c7ba29c
Maynkara08 <mayunksingh2000@gmail.com>
2024-10-18 12:17:16 +0530

    configuration of Postgres Database to store Rules

---
+++ zeotap_rule_engine/src/main/java/com/zeotap/zeotap_rule_engine/repositories/NodeRepository.java
@@ -0,0 +1,9 @@
+package com.zeotap.zeotap_rule_engine.repositories;
+
+import com.zeotap.zeotap_rule_engine.models.ASTNode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class NodeRepository {
+}
